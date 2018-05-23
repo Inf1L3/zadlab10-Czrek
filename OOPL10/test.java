140912fa4d5c5e5dbe804181a5cd0e528a448191
Czrek <37151199+Czrek@users.noreply.github.com>
2018-05-23 09:11:27 +0200

    Add files via upload

---
+++ OOPL10/test.java
@@ -0,0 +1,16 @@
+package javaapplication10;
+ 
+ public class test {
+ 
+     public static void main(String[] args) {
+       
+        
+        z5.zapiszString("a1b2c3");
+        z5.odczytajString();
+        z5.zapiszInt(123);
+        z5.odczytajInt();
+        z5.zapiszDouble(1.12323532543643);
+        z5.odczytajDouble();
+     }
+ 
+ }
